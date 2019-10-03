4cace42bb821c54bd268c245e3b90caa4c425274
Harry Chen <i@harrychen.xyz>
2019-10-03 23:15:54 +0800

    Add more testcases for faintness
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/submit/TestFaintness.java
+++ src/submit/TestFaintness.java
@@ -17,9 +17,66 @@ class TestFaintness {
      * Run the test from root dir using
      * ./run.sh flow.Flow submit.MySolver submit.Faintness submit.TestFaintness
      */
-    /*
-      void test2() {
-      }
-      ...
-    */
+
+    int testReturn() {
+        int x = Integer.valueOf(2);
+        int y = x + 2;
+        // here x (R3), y (R4) are not faint
+        int z = x + y;
+        // here z (R5) is not faint
+        return z;
+    }
+
+    // test the side effect (function call) that prevents faintness
+    int testSideEffect() {
+        int x = Integer.valueOf(233);
+        int y = Integer.valueOf(234); // make sure that y (R4) is not optimized
+        // here x and y are not faint
+        System.out.println(x);
+        // here everything except y are faint
+        return y;
+    }
+
+    // due to optimization, everything variable is fainted in this test
+    int testOptimization() {
+        int y = Integer.valueOf(2);
+        // here y (R3) is faint
+        int z = 1 + y;
+        // this statement faints y
+        z = 0;
+        // here z is fainted (constant is folded)
+        return z;
+    }
+
+    // test the divergence in control flow
+    int testControlFlow() {
+        int x = Integer.valueOf(1); // x is R3
+        int y = Integer.valueOf(2); // y is R4
+        // here both x and y are not fainted
+        if (Integer.valueOf(2333) == 2334) {
+            // here x and y are not faint
+            x = y + 1;
+            // here y is faint
+        } else {
+            // here y is faint
+            x = 5;
+        }
+        // here only x is not faint
+        return x;
+    }
+
+    // test more divergences
+    int testComplexControlFlow() {
+        int x = Integer.valueOf(1); // x is R3
+        int y = Integer.valueOf(2); // y is R4
+        // anywhere in this loop, x and y should not be faint
+        while (x < 5) {
+            x = x + y;
+            if (y < 3) {
+                y = y + 1;
+            }
+        }
+        // here x is not faint
+        return x;
+    }
 }
